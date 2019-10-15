2824dbf30f6c1481d9f10b4719e55b6a78a203fb
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-10-15 18:14:05 +0000

    fixed typo error

--- exercises/01.7-maping-array-of-objects-to-li/index.js
+++ exercises/01.7-maping-array-of-objects-to-li/index.js
@@ -4,11 +4,11 @@ import ReactDOM from "react-dom"; //we use ReactDOM to render into the DOM
 const planets = ["Mars", "Venus", "Jupiter", "Earth", "Saturn", "Neptune"];
 
 /**
- * 1) Create the maping function and use it to generate a new array of
+ * 1) Create the mapping function and use it to generate a new array of
  * planets in html called planetsInHTML
  */
 
 // 2) add the array planetsInHTML inside the innerHTML of this ul
-const content = (<ul className="list-group m-5"></ul>);
+const content = <ul className="list-group m-5"></ul>;
 
 ReactDOM.render(content, document.querySelector("#myDiv"));
